94361d2b63a10fc6d8f785f7aa347e69924989d5
leoneparise <leone.parise@gmail.com>
2019-08-06 18:17:31 -0300

    Add cosmos samples

--- packages/example/src/Provider.tsx
+++ packages/example/src/Provider.tsx
@@ -4,6 +4,7 @@ import Button from "react-bootstrap/Button";
 import Badge from "react-bootstrap/Badge";
 
 import { TrustProvider } from "@trustwallet/provider";
+import { IAccount } from "@trustwallet/types";
 
 interface IState {
   getAccountsLoading: boolean,
@@ -24,6 +25,17 @@ export default class Provider extends React.Component<IProps, IState> {
     };
   }
 
+  private async getAccount(network: number): Promise<IAccount> {
+    let accounts = await TrustProvider.getAccounts()
+      .then(list => list.filter(item => item.network === network));
+
+    if (accounts.length < 1) {
+      throw "Account no found";
+    }
+
+    return accounts[0];
+  }
+
   testGetAccounts = async () => {
     let { openModal } = this.props;
 
@@ -35,35 +47,141 @@ export default class Provider extends React.Component<IProps, IState> {
     }
   };
 
-  testSignTransaction = async () => {
+  testCosmosSendCoins = async () => {
+    let { openModal } = this.props;
+
+    try {
+      let network = 118; // Cosmos
+      let account = await this.getAccount(network);
+      let transaction = {
+          accountNumber: "1035",
+          chainId: "cosmoshub-2",
+          fee: {
+            amounts: [
+              {
+                denom: "uatom",
+                amount: "5000"
+              }
+            ],
+            gas: "200000"
+          },
+          sequence: "40",
+          sendCoinsMessage: {
+            fromAddress: account.address,
+            toAddress: "cosmos1zcax8gmr0ayhw2lvg6wadfytgdhen25wrxunxa",
+            amounts: [
+              {
+                denom: "uatom",
+                amount: "100000"
+              }
+            ]
+          }
+        };
+
+      let result = await TrustProvider.signTransaction(network, transaction);
+      openModal("Transaction Signed", <samp>{result}</samp>);
+    } catch (e) {
+      openModal("Sign Error", <div>{e}</div>);
+    }
+  };
+
+  testCosmosStake = async () => {
     let { openModal } = this.props;
 
     try {
       let network = 118; // Cosmos
       let transaction = {
-          "accountNumber": "1035",
-          "chainId": "cosmoshub-2",
-          "fee": {
-            "amounts": [
-              {
-                "denom": "uatom",
-                "amount": "5000"
-              }
-            ],
-            "gas": "200000"
-          },
-          "sequence": "40",
-          "sendCoinsMessage": {
-            "fromAddress": "cosmos135qla4294zxarqhhgxsx0sw56yssa3z0f78pm0",
-            "toAddress": "cosmos1zcax8gmr0ayhw2lvg6wadfytgdhen25wrxunxa",
-            "amounts": [
-              {
-                "denom": "uatom",
-                "amount": "100000"
-              }
-            ]
-          }
-        };
+        accountNumber: "1035",
+        chainId: "cosmoshub-2",
+        fee: {
+          amounts: [
+            {
+              denom: "uatom",
+              amount: "5000"
+            }
+          ],
+          gas: "200000"
+        },
+        sequence: "40",
+        stakeMessage: {
+          delegatorAddress: "cosmos1hsk6jryyqjfhp5dhc55tc9jtckygx0eph6dd02",
+          validatorAddress: "cosmosvaloper1zkupr83hrzkn3up5elktzcq3tuft8nxsmwdqgp",
+          amounts: [
+            {
+              denom: "uatom",
+              amount: "100000"
+            }
+          ]
+        }
+      };
+
+      let result = await TrustProvider.signTransaction(network, transaction);
+      openModal("Transaction Signed", <samp>{result}</samp>);
+    } catch (e) {
+      openModal("Sign Error", <div>{e}</div>);
+    }
+  };
+
+  testCosmosUnstake = async () => {
+    let { openModal } = this.props;
+
+    try {
+      let network = 118; // Cosmos
+      let transaction = {
+        accountNumber: "1035",
+        chainId: "cosmoshub-2",
+        fee: {
+          amounts: [
+            {
+              denom: "uatom",
+              amount: "5000"
+            }
+          ],
+          gas: "200000"
+        },
+        sequence: "40",
+        stakeMessage: {
+          delegatorAddress: "cosmos1hsk6jryyqjfhp5dhc55tc9jtckygx0eph6dd02",
+          validatorAddress: "cosmosvaloper1zkupr83hrzkn3up5elktzcq3tuft8nxsmwdqgp",
+          amounts: [
+            {
+              denom: "uatom",
+              amount: "100000"
+            }
+          ]
+        }
+      };
+
+      let result = await TrustProvider.signTransaction(network, transaction);
+      openModal("Transaction Signed", <samp>{result}</samp>);
+    } catch (e) {
+      openModal("Sign Error", <div>{e}</div>);
+    }
+  };
+
+  testCosmosWithdraw = async () => {
+    let { openModal } = this.props;
+
+    try {
+      let network = 118; // Cosmos
+      let transaction = {
+        accountNumber: "1035",
+        chainId: "cosmoshub-2",
+        fee: {
+          amounts: [
+            {
+              denom: "uatom",
+              amount: "5000"
+            }
+          ],
+          gas: "200000"
+        },
+        sequence: "40",
+        withdrawStakeRewardMessage: {
+          delegatorAddress: "cosmos1hsk6jryyqjfhp5dhc55tc9jtckygx0eph6dd02",
+          validatorAddress: "cosmosvaloper1zkupr83hrzkn3up5elktzcq3tuft8nxsmwdqgp",
+        }
+      };
 
       let result = await TrustProvider.signTransaction(network, transaction);
       openModal("Transaction Signed", <samp>{result}</samp>);
@@ -77,15 +195,27 @@ export default class Provider extends React.Component<IProps, IState> {
   }
 
   render () {
-    return <Card>
-      <Card.Header>Provider Examples</Card.Header>
-      <Card.Body>
-        <h2>
-          Trust Provider is <Badge variant={ this.isAvailable ? "success" : "danger" }>{this.isAvailable ? "Available" : "Not Available"}</Badge>
-        </h2>
-        <Button block size="lg" disabled={!this.isAvailable} onClick={this.testGetAccounts}>Get Accounts</Button>
-        <Button block size="lg" disabled={!this.isAvailable} onClick={this.testSignTransaction}>Sign Transaction</Button>
-      </Card.Body>
-    </Card>
+    return (
+      <>
+        <Card>
+        <Card.Header>Provider Examples</Card.Header>
+        <Card.Body>
+          <Card.Title>
+            Trust Provider <Badge variant={ this.isAvailable ? "success" : "danger" }>{this.isAvailable ? "is available" : "is not Available"}</Badge>
+          </Card.Title>
+          <Button size="lg" disabled={!this.isAvailable} onClick={this.testGetAccounts}>Get Accounts</Button>
+        </Card.Body>
+      </Card>
+      <Card>
+        <Card.Header>Sign Cosmos Transaction Examples</Card.Header>
+        <Card.Body>
+          <Button size="lg" disabled={!this.isAvailable} onClick={this.testCosmosSendCoins}>Send Coins</Button>
+          <Button size="lg" disabled={!this.isAvailable} onClick={this.testCosmosStake}>Stake</Button>
+          <Button size="lg" disabled={!this.isAvailable} onClick={this.testCosmosUnstake}>Unstake</Button>
+          <Button size="lg" disabled={!this.isAvailable} onClick={this.testCosmosWithdraw}>Withdraw</Button>
+        </Card.Body>
+      </Card>
+    </>
+    )
   }
 }
