b17ed2fb2d9aa421027dd6c59d700477579237ff
Will Pankiewicz <9498646+wpank@users.noreply.github.com>
2019-08-09 02:48:25 -0700

    add eslint and prettier (#28)
    The following adds an esling-config package, from which every
    package extends it's own version
    
    adds commands for linting and checking formatting with prettier

--- packages/example/src/App.tsx
+++ packages/example/src/App.tsx
@@ -1,90 +1,98 @@
 import React from 'react';
-import Navbar from "react-bootstrap/Navbar";
-import Nav from "react-bootstrap/Nav";
-import Container from "react-bootstrap/Container";
-import { LinkContainer } from "react-router-bootstrap";
-import {
-    BrowserRouter as Router,
-    Route,
-} from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import ProviderExamples from "./ProviderExamples";
-import WalletConnectExamples from "./WalletConnectExamples";
-import Modal from "react-bootstrap/Modal";
-import Button from "react-bootstrap/Button";
+import ProviderExamples from './ProviderExamples';
+import WalletConnectExamples from './WalletConnectExamples';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
 
-interface IState {
-  isModalVisible: boolean,
-  modalTitle?: string,
-  modalBody?: React.ReactNode
+interface State {
+    isModalVisible: boolean;
+    modalTitle?: string;
+    modalBody?: React.ReactNode;
 }
 
-interface IProps {}
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface Props {}
 
-export default class App extends React.Component<IProps, IState> {
-  constructor(props: IProps) {
-    super(props);
-    this.state = {
-      isModalVisible: false
+export default class App extends React.Component<Props, State> {
+    constructor(props: Props) {
+        super(props);
+        this.state = {
+            isModalVisible: false,
+        };
+    }
+
+    openModal = (title: string, body: React.ReactNode): void => {
+        this.setState({
+            isModalVisible: true,
+            modalTitle: title,
+            modalBody: body,
+        });
     };
-  }
 
-  openModal = (title: string, body: React.ReactNode) => {
-    this.setState({
-      isModalVisible: true,
-      modalTitle: title,
-      modalBody: body,
-    })
-  };
+    closeModal = (): void => {
+        this.setState({
+            isModalVisible: false,
+        });
+    };
 
-  closeModal = () => {
-    this.setState({
-       isModalVisible: false
-    });
-  };
+    render(): JSX.Element {
+        return (
+            <>
+                <Router>
+                    <Navbar bg="light" expand="lg">
+                        <Container>
+                            <Navbar.Brand>TrustWallet Examples</Navbar.Brand>
+                            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                            <Navbar.Collapse id="navigation">
+                                <Nav className="mr-auto">
+                                    <LinkContainer exact to="/">
+                                        <Nav.Link>Provider</Nav.Link>
+                                    </LinkContainer>
+                                    <LinkContainer to="/walletconnect">
+                                        <Nav.Link>WalletConnect</Nav.Link>
+                                    </LinkContainer>
+                                </Nav>
+                            </Navbar.Collapse>
+                        </Container>
+                    </Navbar>
+                    <Container className="content">
+                        <Route
+                            exact
+                            path="/"
+                            render={(): JSX.Element => (
+                                <ProviderExamples closeModal={this.closeModal} openModal={this.openModal} />
+                            )}
+                        />
+                        <Route
+                            path="/walletconnect"
+                            render={(): JSX.Element => (
+                                <WalletConnectExamples closeModal={this.closeModal} openModal={this.openModal} />
+                            )}
+                        />
+                    </Container>
+                </Router>
 
-  render () {
-    return (
-      <>
-        <Router>
-          <Navbar bg="light" expand="lg" >
-            <Container>
-              <Navbar.Brand>TrustWallet Examples</Navbar.Brand>
-              <Navbar.Toggle aria-controls="basic-navbar-nav" />
-              <Navbar.Collapse id="navigation">
-                <Nav className="mr-auto">
-                  <LinkContainer exact to="/">
-                    <Nav.Link>Provider</Nav.Link>
-                  </LinkContainer>
-                  <LinkContainer to="/walletconnect">
-                    <Nav.Link>WalletConnect</Nav.Link>
-                  </LinkContainer>
-                </Nav>
-              </Navbar.Collapse>
-            </Container>
-          </Navbar>
-          <Container className="content">
-            <Route exact path="/" render={() => <ProviderExamples closeModal={this.closeModal} openModal={this.openModal}/> } />
-            <Route path="/walletconnect" render={() => <WalletConnectExamples closeModal={this.closeModal} openModal={this.openModal}/> } />
-          </Container>
-        </Router>
-
-        <Modal centered show={this.state.isModalVisible} onHide={this.closeModal} scrollable>
-          <Modal.Header closeButton>
-            <Modal.Title>{this.state.modalTitle}</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <p>{this.state.modalBody}</p>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button variant="primary" onClick={this.closeModal}>
-              Close
-            </Button>
-          </Modal.Footer>
-        </Modal>
-      </>
-    );
-  }
+                <Modal centered show={this.state.isModalVisible} onHide={this.closeModal} scrollable>
+                    <Modal.Header closeButton>
+                        <Modal.Title>{this.state.modalTitle}</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <p>{this.state.modalBody}</p>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="primary" onClick={this.closeModal}>
+                            Close
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+            </>
+        );
+    }
 }
-
--- packages/example/src/WalletConnectExamples.tsx
+++ packages/example/src/WalletConnectExamples.tsx
@@ -1,277 +1,295 @@
-import * as React from 'react'
-import Card from "react-bootstrap/Card";
-import Badge from "react-bootstrap/Badge";
-import Button from "react-bootstrap/Button";
-import WalletConnect from "@trustwallet/walletconnect";
-import WalletConnectQRCodeModal from "@walletconnect/qrcode-modal";
-import {IAccount} from "@trustwallet/types";
+import * as React from 'react';
+import Card from 'react-bootstrap/Card';
+import Badge from 'react-bootstrap/Badge';
+import Button from 'react-bootstrap/Button';
+import WalletConnect from '@trustwallet/walletconnect';
+import WalletConnectQRCodeModal from '@walletconnect/qrcode-modal';
+import { Account } from '@trustwallet/types';
 
-interface IState {
-  isConnected: boolean
+interface State {
+    isConnected: boolean;
 }
 
-interface IProps {
-  openModal(title: string, body: React.ReactNode): void,
-  closeModal(): void,
+interface Props {
+    openModal(title: string, body: React.ReactNode): void;
+    closeModal(): void;
 }
 
-export default class WalletConnectExamples extends React.Component<IProps, IState> {
-  private walletConnector:WalletConnect = new WalletConnect({
-    bridge: "https://bridge.walletconnect.org" // Required
-  });
+export default class WalletConnectExamples extends React.Component<Props, State> {
+    private walletConnector: WalletConnect = new WalletConnect({
+        bridge: 'https://bridge.walletconnect.org', // Required
+    });
 
-  constructor(props: IProps) {
-    super(props);
-    this.state = {
-      isConnected: false
+    constructor(props: Props) {
+        super(props);
+        this.state = {
+            isConnected: false,
+        };
+    }
+
+    private createConnector(): void {
+        this.walletConnector = new WalletConnect({
+            bridge: 'https://bridge.walletconnect.org', // Required
+        });
+    }
+
+    private connect = async (): Promise<void> => {
+        // Check if connection is already established
+        if (!this.walletConnector.connected) {
+            this.createConnector();
+            // create new session
+            await this.walletConnector.createSession();
+            // get uri for QR Code modal
+            const uri = this.walletConnector.uri;
+            // display QR Code modal
+            WalletConnectQRCodeModal.open(uri, () => {
+                console.log('QR Code Modal closed');
+            });
+
+            this.subscribe();
+        } else {
+            await this.walletConnector.killSession();
+        }
     };
-  }
 
-  private createConnector() {
-    this.walletConnector = new WalletConnect({
-      bridge: "https://bridge.walletconnect.org" // Required
-    });
-  }
-
-  private connect = async () => {
-    // Check if connection is already established
-    if (!this.walletConnector.connected) {
-      this.createConnector();
-      // create new session
-      await this.walletConnector.createSession();
-      // get uri for QR Code modal
-      const uri = this.walletConnector.uri;
-      // display QR Code modal
-      WalletConnectQRCodeModal.open(uri, () => {
-        console.log('QR Code Modal closed')
-      });
-
-      this.subscribe();
-    } else {
-      await this.walletConnector.killSession();
-    }
-  };
-
-  private subscribe() {
-    // Subscribe to connection events
-    this.walletConnector.on("connect", (error, payload) => {
-      if (error) {
-        console.log("Error on connect:" + error);
-      }
-      // Close QR Code Modal
-      WalletConnectQRCodeModal.close();
-      this.setState({
-        isConnected: true
-      })
-    });
-
-    this.walletConnector.on("session_update", (error, payload) => {
-      if (error) {
-        console.log("Error on seccion_update:" + error);
-      }
-      console.log(payload);
-    });
-
-    this.walletConnector.on("disconnect", (error, payload) => {
-      if (error) {
-        console.log("Error on disconnect:" + error);
-      }
-      console.log(payload);
-      this.setState({
-        isConnected: false
-      })
-    });
-  }
-
-  private async getAccount(network: number): Promise<IAccount> {
-    let accounts = await this.walletConnector.getAccounts()
-      .then(list => list.filter(item => item.network === network));
-
-    if (accounts.length < 1) {
-      throw "Account no found";
-    }
-
-    return accounts[0];
-  }
-
-  testGetAccounts = async () => {
-    let { openModal } = this.props;
-
-    try {
-      let accounts = await this.walletConnector.getAccounts();
-      openModal("Accounts", <samp>{JSON.stringify(accounts)}</samp>);
-    } catch (e) {
-      openModal("Accounts Error", <div>{e}</div>);
-    }
-  };
-
-  testCosmosSendCoins = async () => {
-    let { openModal } = this.props;
-
-    try {
-      let network = 118; // Cosmos
-      let account = await this.getAccount(network);
-      let transaction = {
-        accountNumber: "1035",
-        chainId: "cosmoshub-2",
-        fee: {
-          amounts: [
-            {
-              denom: "uatom",
-              amount: "5000"
+    private subscribe(): void {
+        // Subscribe to connection events
+        this.walletConnector.on('connect', (error: string) => {
+            if (error) {
+                console.log('Error on connect:' + error);
             }
-          ],
-          gas: "200000"
-        },
-        sequence: "40",
-        sendCoinsMessage: {
-          fromAddress: account.address,
-          toAddress: "cosmos1zcax8gmr0ayhw2lvg6wadfytgdhen25wrxunxa",
-          amounts: [
-            {
-              denom: "uatom",
-              amount: "100000"
+            // Close QR Code Modal
+            WalletConnectQRCodeModal.close();
+            this.setState({
+                isConnected: true,
+            });
+        });
+
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        this.walletConnector.on('session_update', (error: string, payload: any) => {
+            if (error) {
+                console.log('Error on seccion_update:' + error);
             }
-          ]
+            console.log(payload);
+        });
+
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        this.walletConnector.on('disconnect', (error: string, payload: any) => {
+            if (error) {
+                console.log('Error on disconnect:' + error);
+            }
+            console.log(payload);
+            this.setState({
+                isConnected: false,
+            });
+        });
+    }
+
+    private async getAccount(network: number): Promise<Account> {
+        let accounts = await this.walletConnector
+            .getAccounts()
+            .then((list: Account[]) => list.filter(item => item.network === network));
+
+        if (accounts.length < 1) {
+            throw 'Account no found';
         }
-      };
 
-      let result = await this.walletConnector.trustSignTransaction(network, transaction);
-      openModal("Cosmos Withdraw Signed", <samp>{result}</samp>);
-    } catch (e) {
-      openModal("Sign Error", <div>{e}</div>);
+        return accounts[0];
     }
-  };
 
-  testCosmosStake = async () => {
-    let { openModal } = this.props;
+    testGetAccounts = async (): Promise<void> => {
+        let { openModal } = this.props;
 
-    try {
-      let network = 118; // Cosmos
-      let transaction = {
-        typePrefix: "auth/StdTx",
-        accountNumber: "1035",
-        chainId: "cosmoshub-2",
-        fee: {
-          amounts: [
-            {
-              denom: "uatom",
-              amount: "5000"
-            }
-          ],
-          gas: "200000"
-        },
-        sequence: "40",
-        stakeMessage: {
-          delegatorAddress: "cosmos1hsk6jryyqjfhp5dhc55tc9jtckygx0eph6dd02",
-          validatorAddress: "cosmosvaloper1zkupr83hrzkn3up5elktzcq3tuft8nxsmwdqgp",
-          amount: {
-            denom: "uatom",
-            amount: "100000"
-          }
+        try {
+            let accounts = await this.walletConnector.getAccounts();
+            openModal('Accounts', <samp>{JSON.stringify(accounts)}</samp>);
+        } catch (e) {
+            openModal('Accounts Error', <div>{e}</div>);
         }
-      };
+    };
 
-      let result = await this.walletConnector.trustSignTransaction(network, transaction);
-      openModal("Cosmos Stake Signed", <samp>{result}</samp>);
-    } catch (e) {
-      openModal("Sign Error", <div>{e}</div>);
-    }
-  };
+    testCosmosSendCoins = async (): Promise<void> => {
+        let { openModal } = this.props;
 
-  testCosmosUnstake = async () => {
-    let { openModal } = this.props;
+        try {
+            let network = 118; // Cosmos
+            let account = await this.getAccount(network);
+            let transaction = {
+                accountNumber: '1035',
+                chainId: 'cosmoshub-2',
+                fee: {
+                    amounts: [
+                        {
+                            denom: 'uatom',
+                            amount: '5000',
+                        },
+                    ],
+                    gas: '200000',
+                },
+                sequence: '40',
+                sendCoinsMessage: {
+                    fromAddress: account.address,
+                    toAddress: 'cosmos1zcax8gmr0ayhw2lvg6wadfytgdhen25wrxunxa',
+                    amounts: [
+                        {
+                            denom: 'uatom',
+                            amount: '100000',
+                        },
+                    ],
+                },
+            };
 
-    try {
-      let network = 118; // Cosmos
-      let transaction = {
-        typePrefix: "auth/StdTx",
-        accountNumber: "1035",
-        chainId: "cosmoshub-2",
-        fee: {
-          amounts: [
-            {
-              denom: "uatom",
-              amount: "5000"
-            }
-          ],
-          gas: "200000"
-        },
-        sequence: "40",
-        unstakeMessage: {
-          delegatorAddress: "cosmos1hsk6jryyqjfhp5dhc55tc9jtckygx0eph6dd02",
-          validatorAddress: "cosmosvaloper1zkupr83hrzkn3up5elktzcq3tuft8nxsmwdqgp",
-          amount: {
-            denom: "uatom",
-            amount: "100000"
-          }
+            let result = await this.walletConnector.trustSignTransaction(network, transaction);
+            openModal('Cosmos Withdraw Signed', <samp>{result}</samp>);
+        } catch (e) {
+            openModal('Sign Error', <div>{e}</div>);
         }
-      };
+    };
 
-      let result = await this.walletConnector.trustSignTransaction(network, transaction);
-      openModal("Cosmos Unstake Signed", <samp>{result}</samp>);
-    } catch (e) {
-      openModal("Sign Error", <div>{e}</div>);
-    }
-  };
+    testCosmosStake = async (): Promise<void> => {
+        let { openModal } = this.props;
 
-  testCosmosWithdraw = async () => {
-    let { openModal } = this.props;
+        try {
+            let network = 118; // Cosmos
+            let transaction = {
+                typePrefix: 'auth/StdTx',
+                accountNumber: '1035',
+                chainId: 'cosmoshub-2',
+                fee: {
+                    amounts: [
+                        {
+                            denom: 'uatom',
+                            amount: '5000',
+                        },
+                    ],
+                    gas: '200000',
+                },
+                sequence: '40',
+                stakeMessage: {
+                    delegatorAddress: 'cosmos1hsk6jryyqjfhp5dhc55tc9jtckygx0eph6dd02',
+                    validatorAddress: 'cosmosvaloper1zkupr83hrzkn3up5elktzcq3tuft8nxsmwdqgp',
+                    amount: {
+                        denom: 'uatom',
+                        amount: '100000',
+                    },
+                },
+            };
 
-    try {
-      let network = 118; // Cosmos
-      let transaction = {
-        typePrefix: "auth/StdTx",
-        accountNumber: "1035",
-        chainId: "cosmoshub-2",
-        fee: {
-          amounts: [
-            {
-              denom: "uatom",
-              amount: "5000"
-            }
-          ],
-          gas: "200000"
-        },
-        sequence: "40",
-        withdrawStakeRewardMessage: {
-          delegatorAddress: "cosmos1hsk6jryyqjfhp5dhc55tc9jtckygx0eph6dd02",
-          validatorAddress: "cosmosvaloper1zkupr83hrzkn3up5elktzcq3tuft8nxsmwdqgp",
+            let result = await this.walletConnector.trustSignTransaction(network, transaction);
+            openModal('Cosmos Stake Signed', <samp>{result}</samp>);
+        } catch (e) {
+            openModal('Sign Error', <div>{e}</div>);
         }
-      };
+    };
 
-      let result = await this.walletConnector.trustSignTransaction(network, transaction);
-      openModal("Cosmos Withdraw Signed", <samp>{result}</samp>);
-    } catch (e) {
-      openModal("Sign Error", <div>{e}</div>);
+    testCosmosUnstake = async (): Promise<void> => {
+        let { openModal } = this.props;
+
+        try {
+            let network = 118; // Cosmos
+            let transaction = {
+                typePrefix: 'auth/StdTx',
+                accountNumber: '1035',
+                chainId: 'cosmoshub-2',
+                fee: {
+                    amounts: [
+                        {
+                            denom: 'uatom',
+                            amount: '5000',
+                        },
+                    ],
+                    gas: '200000',
+                },
+                sequence: '40',
+                unstakeMessage: {
+                    delegatorAddress: 'cosmos1hsk6jryyqjfhp5dhc55tc9jtckygx0eph6dd02',
+                    validatorAddress: 'cosmosvaloper1zkupr83hrzkn3up5elktzcq3tuft8nxsmwdqgp',
+                    amount: {
+                        denom: 'uatom',
+                        amount: '100000',
+                    },
+                },
+            };
+
+            let result = await this.walletConnector.trustSignTransaction(network, transaction);
+            openModal('Cosmos Unstake Signed', <samp>{result}</samp>);
+        } catch (e) {
+            openModal('Sign Error', <div>{e}</div>);
+        }
+    };
+
+    testCosmosWithdraw = async (): Promise<void> => {
+        let { openModal } = this.props;
+
+        try {
+            let network = 118; // Cosmos
+            let transaction = {
+                typePrefix: 'auth/StdTx',
+                accountNumber: '1035',
+                chainId: 'cosmoshub-2',
+                fee: {
+                    amounts: [
+                        {
+                            denom: 'uatom',
+                            amount: '5000',
+                        },
+                    ],
+                    gas: '200000',
+                },
+                sequence: '40',
+                withdrawStakeRewardMessage: {
+                    delegatorAddress: 'cosmos1hsk6jryyqjfhp5dhc55tc9jtckygx0eph6dd02',
+                    validatorAddress: 'cosmosvaloper1zkupr83hrzkn3up5elktzcq3tuft8nxsmwdqgp',
+                },
+            };
+
+            let result = await this.walletConnector.trustSignTransaction(network, transaction);
+            openModal('Cosmos Withdraw Signed', <samp>{result}</samp>);
+        } catch (e) {
+            openModal('Sign Error', <div>{e}</div>);
+        }
+    };
+
+    render(): JSX.Element {
+        let { isConnected } = this.state;
+
+        return (
+            <>
+                <Card>
+                    <Card.Header>WalletConnect Examples</Card.Header>
+                    <Card.Body>
+                        <Card.Title>
+                            WalletConnect{' '}
+                            <Badge variant={isConnected ? 'success' : 'danger'}>
+                                {isConnected ? 'is connected' : 'is not connected'}
+                            </Badge>
+                        </Card.Title>
+                        <Button size="lg" onClick={this.connect}>
+                            {isConnected ? 'Disconnect' : 'Connect'}
+                        </Button>
+                        <Button size="lg" disabled={!isConnected} onClick={this.testGetAccounts}>
+                            Get Accounts
+                        </Button>
+                    </Card.Body>
+                </Card>
+                <Card>
+                    <Card.Header>Sign Cosmos Transaction Examples</Card.Header>
+                    <Card.Body>
+                        <Button size="lg" disabled={!isConnected} onClick={this.testCosmosSendCoins}>
+                            Send Coins
+                        </Button>
+                        <Button size="lg" disabled={!isConnected} onClick={this.testCosmosStake}>
+                            Stake
+                        </Button>
+                        <Button size="lg" disabled={!isConnected} onClick={this.testCosmosUnstake}>
+                            Unstake
+                        </Button>
+                        <Button size="lg" disabled={!isConnected} onClick={this.testCosmosWithdraw}>
+                            Withdraw
+                        </Button>
+                    </Card.Body>
+                </Card>
+            </>
+        );
     }
-  };
-
-  render () {
-    let { isConnected } = this.state;
-
-    return (
-      <>
-        <Card>
-          <Card.Header>WalletConnect Examples</Card.Header>
-          <Card.Body>
-            <Card.Title>
-              WalletConnect <Badge variant={ isConnected ? "success" : "danger" }>{isConnected ? "is connected" : "is not connected"}</Badge>
-            </Card.Title>
-            <Button size="lg" onClick={this.connect}>{ isConnected ? "Disconnect" : "Connect" }</Button>
-            <Button size="lg" disabled={!isConnected} onClick={this.testGetAccounts}>Get Accounts</Button>
-          </Card.Body>
-        </Card>
-        <Card>
-          <Card.Header>Sign Cosmos Transaction Examples</Card.Header>
-          <Card.Body>
-            <Button size="lg" disabled={!isConnected} onClick={this.testCosmosSendCoins}>Send Coins</Button>
-            <Button size="lg" disabled={!isConnected} onClick={this.testCosmosStake}>Stake</Button>
-            <Button size="lg" disabled={!isConnected} onClick={this.testCosmosUnstake}>Unstake</Button>
-            <Button size="lg" disabled={!isConnected} onClick={this.testCosmosWithdraw}>Withdraw</Button>
-          </Card.Body>
-        </Card>
-      </>
-    )
-  }
 }
