7154fe123a398bcdd58344007791c21342bb6ec9
Leone Parise <leone.parise@gmail.com>
2019-08-26 17:43:44 +0200

    Blockatlas integration (#72)
    * Add blockatlas integration
    
    * Fix CI

---
+++ packages/rpc/src/blockatlas/models/BlockatlasTransaction.ts
@@ -0,0 +1,36 @@
+import { Type, Transform } from 'class-transformer';
+import BigNumber from 'bignumber.js';
+import { BlockatlasResult } from './BlockatlasResult';
+
+class Metadata {
+    @Transform(value => new BigNumber(value), { toClassOnly: true })
+    value: BigNumber;
+    symbol: string;
+    decimals: number;
+    title?: string;
+    key?: string;
+    name?: string;
+}
+
+export class BlockatlasTransaction {
+    id: string;
+    coin: number;
+    from: string;
+    to: string;
+    @Transform(value => new BigNumber(value), { toClassOnly: true })
+    fee: BigNumber;
+    @Transform(value => new BigNumber(value), { toClassOnly: true })
+    block: BigNumber;
+    @Transform(value => new Date(value * 1000), { toClassOnly: true })
+    date: Date;
+    status: string;
+    type: string;
+    memo: string;
+    @Type(() => Metadata)
+    metadata: Metadata;
+}
+
+export class BlockatlasTransactionResult extends BlockatlasResult {
+    @Type(() => BlockatlasTransaction)
+    docs: BlockatlasTransaction[];
+}
--- packages/rpc/src/index.ts
+++ packages/rpc/src/index.ts
@@ -3,3 +3,5 @@ export { BitcoinFeePriority } from './bitcoin/models/BitcoinFeePriority';
 export * from './bitcoin/models';
 export { CosmosRPC } from './cosmos/CosmosRPC';
 export * from './cosmos/models';
+export { BlockatlasRPC } from './blockatlas/BlockatlasRPC';
+export * from './blockatlas/models';
