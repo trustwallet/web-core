94b70ac62f47c0e1642768e34e7892cc94367632
dpereskokov <dmitry.v.pereskokov@gmail.com>
2019-08-28 19:05:19 +0300

    lints and prettier fix

--- packages/types/.eslintrc.js
+++ packages/types/.eslintrc.js
@@ -1,5 +1,10 @@
 module.exports = {
     extends: [
         "../eslint-config/.eslintrc.js"
-    ]
+    ],
+    rules:  {
+        "@typescript-eslint/no-namespace": 0,
+        "@typescript-eslint/explicit-function-return-type": 0,
+        "@typescript-eslint/no-explicit-any": 0
+    }
 };
