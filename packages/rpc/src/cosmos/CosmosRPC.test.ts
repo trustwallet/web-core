3f903afd70807d3908d2a43f77aa5701bc0db351
dpereskokov <dmitry.v.pereskokov@gmail.com>
2019-09-17 16:12:49 +0300

    fix tests

--- packages/rpc/src/cosmos/CosmosRPC.test.ts
+++ packages/rpc/src/cosmos/CosmosRPC.test.ts
@@ -59,7 +59,7 @@ describe('CosmosRPC', () => {
     });
 
     it('should broadcast transaction', async () => {
-        spyOn(axios, 'post').and.returnValue({ data: '' });
+        spyOn(axios, 'post').and.returnValue({ status: 201 });
         const data =
             '{"tx":{"memo":"","signatures":[{"pub_key":{"type":"tendermint\\/PubKeySecp256k1","value":"ApGCXwby9foj0IAqOYvmjI+Sd2qdGVdyI1h+CiIFY8xF"},"signature":"5je8nZG5k3Qel1LeJ8f0QAZjwaeRK5Uw\\/DOaPHE64MBCAqYYZCO5l\\/mkxLSzQyxJABk14m+gzCNpSNHiWQm84w=="}],"msg":[{"type":"cosmos-sdk\\/MsgSend","value":{"amount":[{"amount":"2241155","denom":"uatom"}],"from_address":"cosmos135qla4294zxarqhhgxsx0sw56yssa3z0f78pm0","to_address":"cosmos1suasadhn8wmueg93u6js8ala89azqwg6fswuln"}}],"type":"cosmos-sdk\\/MsgSend","fee":{"amount":[{"amount":"1000","denom":"uatom"}],"gas":"200000"}},"mode":"async"}';
         await rpc.broadcastTransaction(data);
