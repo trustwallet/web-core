b61debed24d4529f40b375facd536cb252b08870
dpereskokov <dmitry.v.pereskokov@gmail.com>
2019-09-18 18:45:22 +0300

    prettier

--- packages/rpc/src/iotex/IoTexRPC.ts
+++ packages/rpc/src/iotex/IoTexRPC.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { plainToClass } from 'class-transformer';
-import {IoTeXAccount, IoTexBroadcastResult, IoTexTransaction} from './models';
+import { IoTeXAccount, IoTexBroadcastResult, IoTexTransaction } from './models';
 import { Query } from './Query';
 import { NetworkError } from '../errors/network-error';
 
@@ -15,26 +15,23 @@ export class IoTexRPC {
         return new Query(this.rpcUrl);
     }
 
-
     async getAccount(address: string): Promise<IoTeXAccount> {
         let response = await axios.get(this.query().getAccount(address));
         return plainToClass(IoTeXAccount, response.data);
     }
-
 
     async getTransactionByHash(hash: string): Promise<IoTexTransaction> {
         let response = await axios.get(this.query().getTransaction(hash));
         return plainToClass(IoTexTransaction, response.data);
     }
 
-
     async broadcastTransaction(data: string): Promise<IoTexBroadcastResult> {
         try {
             const url = this.query().broadcastTransaction(data);
             const options = {
                 headers: {
-                    'Content-Type': 'application/json'
-                }
+                    'Content-Type': 'application/json',
+                },
             };
             const response = await axios.post(url, null, options);
             return plainToClass(IoTexBroadcastResult, response.data);
