308306bce388f0b5ea7b99b203c5ed9ac8640a69
leoneparise <1442927+leoneparise@users.noreply.github.com>
2019-09-18 14:22:24 -0300

    Add tests to types package

--- packages/types/src/crypto/hex.test.ts
+++ packages/types/src/crypto/hex.test.ts
@@ -1,15 +1,15 @@
-import Hex from './hex';
+import { fromHex, toHex } from './hex';
 
 describe('Hex', () => {
    it('should encode data', () => {
       const expected = "0xff000a";
-      const encoded = Hex.encode(new Uint8Array([ 255, 0, 10 ]),  true);
+      const encoded = toHex(new Uint8Array([ 255, 0, 10 ]),  true);
       expect(encoded).toEqual(expected);
    });
 
    it('should decode data', () => {
       const expected = new Uint8Array([ 255, 0, 10 ]);
-      const decoded = Hex.decode("0xff000a");
+      const decoded = fromHex("0xff000a");
       expect(decoded).toEqual(expected);
    });
 });
---
+++ packages/types/src/crypto/hex.ts
@@ -0,0 +1,14 @@
+export function toHex(data: Uint8Array, prefix: boolean = false): string {
+    const encoded = data.reduce((str, byte) => str + byte.toString(16).padStart(2, '0'), '');
+    return prefix ? '0x' + encoded : encoded;
+}
+
+export function fromHex(data: string): Uint8Array {
+    if (data.length % 2 !== 0) {
+        throw new Error('invalid data size')
+    }
+    if (data.indexOf('0x') === 0) {
+        data = data.substring(2);
+    }
+    return new Uint8Array(data.match(/.{1,2}/g).map(byte => parseInt(byte, 16)));
+}
