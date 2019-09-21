e0de3431fd1dfcceab5db9488d0d0f301a80cb39
leoneparise <1442927+leoneparise@users.noreply.github.com>
2019-09-21 02:50:07 -0300

    Small refactoring

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -1,7 +1,7 @@
 // created from 'create-ts-index'
-import * as utils from './utils';
+import * as Utils from './utils';
 export * from './Account';
 export * from './CoinType';
 export * from './Cosmos';
 export * from './FiatCoinType';
-export { utils };
+export { Utils };
