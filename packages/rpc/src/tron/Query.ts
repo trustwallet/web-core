8aa82a49324ef0808f16a887fbbf475c95d2662c
leoneparise <1442927+leoneparise@users.noreply.github.com>
2019-11-08 11:21:07 -0300

    Fix Tron RPC path

--- packages/rpc/src/tron/Query.ts
+++ packages/rpc/src/tron/Query.ts
@@ -5,19 +5,19 @@ export class Query {
         this.rpcUrl = rpcUrl;
     }
 
-    getNowBlock = () => this.uri('/wallet/getnowblock');
+    getNowBlock = () => this.uri('wallet/getnowblock');
 
-    getAccount = () => this.uri('/wallet/getaccount');
+    getAccount = () => this.uri('wallet/getaccount');
 
-    broadcastTransaction = () => this.uri('/wallet/broadcasttransaction');
+    broadcastTransaction = () => this.uri('wallet/broadcasttransaction');
 
-    getTransactionById = () => this.uri('/wallet/gettransactionbyid');
+    getTransactionById = () => this.uri('wallet/gettransactionbyid');
 
-    triggerSmartContract = () => this.uri('/wallet/triggersmartcontract');
+    triggerSmartContract = () => this.uri('wallet/triggersmartcontract');
 
-    freezeBalance = () => this.uri('/wallet/freezebalance');
+    freezeBalance = () => this.uri('wallet/freezebalance');
 
-    unfreezeBalance = () => this.uri('/wallet/unfreezebalance');
+    unfreezeBalance = () => this.uri('wallet/unfreezebalance');
 
     private uri(path: string): string {
         return `${this.rpcUrl}/${path}`;
